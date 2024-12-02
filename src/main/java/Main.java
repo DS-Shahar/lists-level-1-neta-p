324f8944703c7d14e377acf8df1eea0134fa0b75
neta-p <netapernat@gmail.com>
2024-12-02 14:47:50 +0200

    Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,44 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+package list;
+
+public class list1 {
+
+	    public static void main(String[] args) {
+	        int x = 1, y = 10, n=30;
+	        Node<Integer> head = generateRandomNumbers(x, y, n);
+	        System.out.println("Generated List: " + head);
+
+	        int numberToCount = 5;
+	        int occurrences = countOccurrences(head, numberToCount);
+	        System.out.println("Number " + numberToCount + " appears " + occurrences + " times.");
+	    }
+
+	    
+	    public static Node<Integer> generateRandomNumbers(int x, int y, int n) {
+	       
+	        Node<Integer> head = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+	        Node<Integer> current = head;
+
+	        for (int i = 1; i < n; i++) {
+	            Node<Integer> newNode = new Node<>((int) (Math.random() * (y - x + 1)) + x);
+	            current.setNext(newNode);
+	            current = newNode;
+	        }
+
+	        return head; 
+	    }
+
+	    
+	    public static int countOccurrences(Node<Integer> head, int target) {
+	        int count = 0;
+	        Node<Integer> current = head;
+
+	        while (current != null) {
+	            if (current.getValue() == target) {
+	                count++;
+	            }
+	            current = current.getNext();
+	        }
+
+	        return count;
+	    }
+	}
